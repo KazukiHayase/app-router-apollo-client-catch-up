76fd0dac89c7913027cf581089a0f51afb171f22
KazukiHayase <kazuki.engineer@gmail.com>
2025-04-27 23:23:34 +0900

    add ApolloClient

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,42 @@
-export default function Home() {
+import gql from "graphql-tag";
+import {getClient} from "./ApolloClient";
+
+const userQuery = gql`
+  query {
+    getUser(id: "1") {
+      id
+      name
+    }
+  }
+`;
+
+const userIdQuery = gql`
+  query {
+    getUser(id: "1") {
+      id
+    }
+  }
+`;
+
+export default async function Home() {
+  const {data} = await getClient().query({query: userQuery});
+  /* const {data} = await getClient().query({query: userIdQuery}); */
+
   return (
     <div>
-      <p>Home</p>
+      <p>data received during Home render: {JSON.stringify(data)}</p>
+      <Child />
     </div>
   );
 }
+
+const Child = async function () {
+  /* const {data} = await getClient().query({query: userQuery}); */
+  const {data} = await getClient().query({query: userIdQuery});
+
+  return (
+    <div>
+      <p>data received during Child render: {JSON.stringify(data)}</p>
+    </div>
+  );
+};
