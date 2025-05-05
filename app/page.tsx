ef816931f3ccba3876e03fdc048180f845b63b9f
KazukiHayase <kazuki.engineer@gmail.com>
2025-05-05 15:02:35 +0900

    rename rest request

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Page() {
     <>
       <Link href="/graphql-single-request">Graphql Single Request Page</Link>
       <Link href="/graphql-multi-request">Graphql Multi Request Page</Link>
-      <Link href="/rest-request">REST Request Page</Link>
+      <Link href="/rest">REST Page</Link>
     </>
   );
 }
