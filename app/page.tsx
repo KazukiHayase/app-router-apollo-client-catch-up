3f52c61431ed1df295f9e3a5c4bf71852fa6bcb4
KazukiHayase <kazuki.engineer@gmail.com>
2025-05-05 18:06:33 +0900

    add graphql page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default async function Page() {
   return (
     <>
+      <Link href="/graphql">Graphql Page</Link>
       <Link href="/graphql-single-request">Graphql Single Request Page</Link>
       <Link href="/graphql-multi-request">Graphql Multi Request Page</Link>
       <Link href="/graphql-cache-option">Graphql Cache Options Page</Link>
