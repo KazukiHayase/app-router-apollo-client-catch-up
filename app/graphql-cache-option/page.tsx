e7b1fbc095eadd164cc13ce299d0360fb8780ed3
KazukiHayase <kazuki.engineer@gmail.com>
2025-05-05 17:35:20 +0900

    add graphql cache page

---
+++ app/graphql-cache-option/page.tsx
@@ -0,0 +1,30 @@
+import gql from "graphql-tag";
+import {getClient} from "@/app/ApolloClient";
+import Link from "next/link";
+
+const userQuery = gql`
+  query {
+    getUser(id: "1") {
+      id
+      name
+    }
+  }
+`;
+
+export default async function Page() {
+  const {data} = await getClient().query({
+    query: userQuery,
+    context: {
+      fetchOptions: {
+        cache: "force-cache",
+      },
+    },
+  });
+
+  return (
+    <>
+      <p>data received during Page render: {JSON.stringify(data)}</p>
+      <Link href="/">Home</Link>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default async function Page() {
     <>
       <Link href="/graphql-single-request">Graphql Single Request Page</Link>
       <Link href="/graphql-multi-request">Graphql Multi Request Page</Link>
+      <Link href="/graphql-cache-option">Graphql Cache Options Page</Link>
       <Link href="/graphql-revalidate">Graphql Revalidate Page</Link>
       <Link href="/rest">REST Page</Link>
     </>
